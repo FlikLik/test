ff9f7d95cf14bc5dea4c691d2c21fee800231214
FlikLik <dorantesrodriguezu@gmail.com>
2025-02-22 20:57:04 -0600

    Cambios desde Lenovo

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+          Editado desde Lenovo
         </p>
       </div>
       <p className="read-the-docs">
